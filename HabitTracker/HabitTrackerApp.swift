ccb173272f823b551ef322eeb03b5626672edd72
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-11-01 15:29:09 +0000

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Miłosz Tabaka on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
